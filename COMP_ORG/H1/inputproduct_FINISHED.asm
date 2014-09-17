30c76650f0798cb34df071e8c2f5a0e05f3d0adc
Jason <jason_downing@student.uml.edu>
2014-09-16 21:57:07 -0400

    FINISHED FOR GOOD.

--- COMP_ORG/H1/inputproduct_FINISHED.asm
+++ COMP_ORG/H1/inputproduct_FINISHED.asm
@@ -1,42 +1,51 @@
+#  ** HW #2 **
 # Author: 	Jason Downing
 #   Date:	9/14/2014
 
-	.data					# Data portion of the program (variables, constants, etc)
+	.data										# Data portion of the program (variables, constants, etc)
 prompt:	.asciiz "Enter the first integer: " 	# Load the first prompt into memory
 prompt2:.asciiz	"Enter the second integer: "  	# Load the second prompt into memory
-prompt3:.asciiz	"The result is: "		# Load the final result msg into memory
+prompt3:.asciiz	"The result is: "				# Load the final result msg into memory
 
 	.text		
 main:	
 	# Code 1 = print int. Code 4 = print string. Code 5 = get int. Code 10 = exit.
 
-	# FIRST PROMPT
+	# C CODE EQUIVALENT
+	# int t0, t1;
+	# printf("Enter two integers: ");
+	# scanf("%d %d", &t0, &t1);
+	# printf("The final result is: %d", t0*t1);
+
+	# FIRST PROMPT 		( printf("Enter the first integer: "); )
 	la	$a0, prompt		# Get the address of the first message.
 	li	$v0, 4			# Print the first prompt out. (Code 4)
 	syscall				# Tell the OS to perform the above operation.
 	
-	# GET FIRST INT
+	# GET FIRST INT 	( scanf("%d", &t0); )
 	li 	$v0, 5			# Read the first int into $v0. (Code 5)
 	syscall				# Tell the OS to perform the above operation.
 	add	$t0, $v0, $0	# Add the first int that was entered into the $t0 register
 	
-	# SECOND PROMPT
+	# SECOND PROMPT 	( printf("Enter the second integer: "); )
 	la	$a0, prompt2	# Get the addr of the 2nd message.
 	li	$v0, 4			# Print the second prompt. (Code  4)
 	syscall				# Tell the OS to perform the above operation.
 	
-	# GET SECOND INT
+	# GET SECOND INT 	( scanf("%d", &t1); )
 	li 	$v0, 5			# Read the second int into $t1. (Code 5)
 	syscall				# Tell the OS to perform the above operation.
 	add	$t1, $v0, $0	# Add the second int that was entered into the $t1 register
 	
-	# MULTIPLY THE TWO INTS
-	mult 	$t0, $t1	# Multiply the two numbers.
-	mfhi	$t2			# Store part of the result of the multiplication to the $t2 register.
-	mflo	$t3			# Store the other part of the result into the $t3 register
-	add	$t4, $t2, $t3	# Add the two parts into the $t4 register.
+	# MULTIPLY THE TWO INTS ( t4 = t0 * t1)
+	mul $t4, $t0, $t1
+
+	#mult 	$t0, $t1	# Multiply the two numbers.
+	#mfhi	$t2			# Store part of the result of the multiplication to the $t2 register.
+	#mflo	$t3			# Store the other part of the result into the $t3 register
+	#add	$t4, $t2, $t3	# Add the two parts into the $t4 register.
 	
-	# FINAL RESULT MESSAGE
+	# FINAL RESULT MESSAGE 	( printf("The result is: "); )
 	la 	$a0, prompt3	# Get the addr of the final prompt.
 	li	$v0, 4			# Print the final prompt. (Code 4)
 	syscall				# Tell the OS to perform the above operation.
