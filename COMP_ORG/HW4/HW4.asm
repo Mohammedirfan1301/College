7f641430808c0c7f42bd1fa272e94f0bd62d6702
Jason <jason_downing@student.uml.edu>
2014-10-13 20:41:34 -0400

    pretty sure I finally finished this homework. Very annoying and long.

--- COMP_ORG/HW4/HW4.asm
+++ COMP_ORG/HW4/HW4.asm
@@ -1,24 +1,23 @@
-# Jason Downing
-# 10/7/2014
+# Jason Downing™ ©2014
+# 10/13/2014
 
                 .data
 inArr:          .word     0:40
 symTab:         .word     0:40
-prompt:         .asciiz   "Enter new set of tokens and types: "
+prompt:         .asciiz   "Enter a new token: "
+prompt2:        .asciiz   "Enter a new  type: "
 dump_prompt:	.asciiz	  "\nDumping symTab: \n\n"
 exit_prompt:	.asciiz	  "\n\nExiting the program..."
 
         .text
 main:
 	# Initialize variables.
-        li      $t0, 0      	# index to inArr
-        li	$t1, 0		# index for symTab
+        li      $t0, 0      	# index to inArr    ( ALSO the MAX number to compare against )
+        li	$t1, 0		# index for symTab  ( ALSO the MAX number to compare against )
         li      $t2, 0 		# DEFN
         li	$t3, 0		# LOC
         li	$t6, 0		# TEMP index for looping, clearing, dumping, etc.
 
-# $s0 -> the value of the current input.
-# $s1 -> Used for comparing individual characters against
 
 # NOTES
 # Input should be a label
@@ -30,31 +29,12 @@ main:
 # The value (one word / 1 integer / 4 bytes)
 # The status (one word / 1 integer / 4 bytes)
 
-#
-# Get one label, followed by a number.
-#
-inLoop:
-        jal     getTokens			# Get one label and one number.
 
-#
-# Check to see what we found.
-#
-chkVar:
-        beq     $t6, 2, label		# 0x32 = decimal #2, which is a label.
-        beq	$t6, 4, control		# 0x34 = decimal #4, which is a colon or comma.
-        beq	$t6, 5, money		# 0x35 = decimal #5, which is the money sign.
-        beq	$t6, 6, pound		# 0x36 = decimal #6, which is the pound sign.
-
-        # If we get here, the user entered something strange so just quit!
-        b exit
-
-#
 # Save one label and its type to the inArr array.
-#
 getTokens:
 	# Print out prompt
-    	la  	$a0, prompt   	# Loads addr of prompt_input into $a0
-    	li  	$v0, 4          # Code 4 means output string.
+    	la  	$a0, prompt   		# Loads addr of prompt_input into $a0
+    	li  	$v0, 4          	# Code 4 means output string.
     	syscall
 
 	# Press enter between the label and the number.
@@ -67,7 +47,12 @@ getTokens:
     	
     	#  Move forward by 8, to be able to save the integer.
     	addi	$t0, $t0, 8
-    	
+    	    	
+	# Print out prompt
+    	la  	$a0, prompt2   		# Loads addr of prompt_input into $a0
+    	li  	$v0, 4         	 	# Code 4 means output string.
+    	syscall
+
     	# Input the number.
 	li	$v0, 5			# Code 5 means input an integer.
 	syscall
@@ -80,12 +65,20 @@ getTokens:
 
 	# Move the array forward by 4. Since we stored a 4 byte integer.
 	addi	$t0, $t0, 4
+	
+	
+# Check to see what we found.
+chkVar:
+        beq     $t6, 2, label		# 0x32 = decimal #2, which is a label.
+        beq	$t6, 4, control		# 0x34 = decimal #4, which is a colon or comma.
+        beq	$t6, 5, money		# 0x35 = decimal #5, which is the money sign.
+        beq	$t6, 6, pound		# 0x36 = decimal #6, which is the pound sign.
 
-	b	chkVar			# Go to chkVar
+        # If we get here, the user entered something strange so just quit!
+        b exit
 
-# 
+
 # We found a label, so save the label & it's value into symTab. Also DEFN = 1.
-#
 label:
 	# Save the two words
 	subi 	$t7, $t0, 12		# Go back 12 spaces.
@@ -109,11 +102,10 @@ label:
 	sw	$v0, symTab($t3)	# Save DEFN into symTab
 	addi	$t3, $t3, 4		# Move LOC forward by 4.
 	
-	b inLoop # Once we've added the label, value & DEFN into symTab, return to inLoop
+	b getTokens 			# Once we've added the label, value & DEFN into symTab, return to getTokens
 
-#
+
 # Found a colon or a comma, so save that + it's value into symTab. Also DEFN = 0.
-#
 control:
 	# Save the comma or colon
 	subi 	$t7, $t0, 12		# Go back 12 spaces.
@@ -137,11 +129,10 @@ control:
 	sw	$v0, symTab($t3)	# Save DEFN into symTab
 	addi	$t3, $t3, 4		# Move LOC forward by 4
 	
-	b inLoop # Once we've added it to symTab, return to inLoop
+	b getTokens 			# Once we've added the label, value & DEFN into symTab, return to getTokens
 
-#
+
 # Found a money sign, so save a "$", its value (5) and DEFN = 0 into symTab.
-#
 money:
 	# Save the money sign
 	subi 	$t7, $t0, 12		# Go back 12 spaces.
@@ -165,11 +156,10 @@ money:
 	sw	$v0, symTab($t3)	# Save DEFN into symTab
 	addi	$t3, $t3, 4		# Move LOC forward by 4.
 	
-	b inLoop # Once we've added it to symTab, return to inLoop
+	b getTokens 			# Once we've added the label, value & DEFN into symTab, return to getTokens
 
-#
+
 # Found the pound sign, so we're done! Save the sign, its value (6) and DEFN = 0.
-#
 pound:
 	# Save the pound sign
 	subi 	$t7, $t0, 12		# Go back 12 spaces.
@@ -193,50 +183,30 @@ pound:
 	sw	$v0, symTab($t3)	# Save DEFN into symTab
 	addi	$t3, $t3, 4		# Move LOC forward by 4.
 
-
 	# FIRST: Print symTab.
 	# SECOND: Clear inArr
 	# FINALLY: exit.
 	
-	# DUMPING SYMBTAB.
     	la  	$a0, dump_prompt   	# Loads address of prompt_input into $a0
     	li  	$v0, 4          	# Sys Call Code 4 means output string.
     	syscall
 
-    	# Load up the symTab array.
+    	# We'll need an index variable and the address of symTab.
     	li	$t1, 0			# index for symTab. LOC ($t3) will be our max number to loop through.
-
-	# Get the address of symTab into $a1
 	la 	$a1, symTab		# Loads address of symTab into $a1.	
         
-        jal     dumpSymTab
+        jal     dumpSymTab		# Go dump the symTab array
         
 
-#
 # dumpSymTab: Print the symTab array to the screen.
-#
 dumpSymTab:
 	beq	$t1, $t3, setUpClear		# Go nuke the array once we're done here.
 	
-	# Output the first word in the symbTab Array.
-    	#lw  	$a0, symTab($t1)   		# Loads the word from symTab into $a0
-    	
+	# Output the first two words in the symbTab Array.
     	li	$t6, 0				# Start at 0, go to 3.
-    	jal 	printWord			# Go print the next four bytes of $a0.
+    	jal 	printWords			# Go print the next eight bytes of $a0.
     	
-    	addi	$a1, $a1, 4			# move forward one word.
-    	addi	$t1, $t1, 4			# move forward one word.
-    	
-    	# Output the second word in the symbTab Array.
-    	#lw  	$a0, symTab($t1)   		# Loads the word from symTab into $a0
-	
-	
-    	#la 	$a1, symTab			# Load address of symTab into $a1.
-	li	$t6, 0				# Start at 0, go to 3.
-    	jal 	printWord			# Go print the next four bytes of $a0.
-    	
-    	addi	$a1, $a1, 4			# move forward one word.
-    	addi	$t1, $t1, 4			# move forward one word.
+    	addi	$t1, $t1, 8			# move forward two words.
     	
     	# Then output the value of the label/variable.
     	lw  	$a0, symTab($t1)   		# Loads addr of prompt_input into $a0
@@ -257,21 +227,9 @@ dumpSymTab:
 	b	dumpSymTab
 	
 	
-#
-# This function will print out 4 bytes - AKA a word.
-#
-printWord:
-	
-	# Next thing to do. Print out 4 bytes.
-	# Something like
-	beq	$t6, 4, returnToFunction	# Once we print out the 4 characters, go back to dumbSymTab
-	
-	
-	# TRY THIS
-	#la $t0, string
-	#lb $a0, ($t0)
-	#li $v0, 11
-	#syscall	
+# This function will print out the first two words (8 bytes),
+printWords:
+	beq	$t6, 8, returnToFunction	# Once we print out the 4 characters, go back to dumbSymTab
 	
 	# Print out one character.
 	lb	$a0, ($a1)
@@ -284,27 +242,19 @@ printWord:
 	# Increase the counter by 1.
 	addi	$t6, $t6, 1
 	
-	b 	printWord
-	
+	b 	printWords
+
+
 returnToFunction:
-	jr	$ra	# Go back to where the jal call is from.
+	jr	$ra			# Go back to where the jal call is from.
 
 
-#
 # Sets up the clearing inArr
-#
 setUpClear:
-	# Clear the array
-	li 	$t6, 0	# Index to use for clearing the array.     	
-
-	# FOR TESTING WE'LL JUST EXIT RATHER THAN CLEAR THE ARRAY!
-        jal	exit
-        #jal     clearInArr
+	li 	$t6, 0			# Index to use for clearing the array.     	
 
 
-#
 # clearInArr: wipes the inArry array.
-#
 clearInArr:
 	beq	$t0, $t6, exit		# done clearing if $t6 is equal to the max number of bytes in inArr
 
@@ -314,10 +264,8 @@ clearInArr:
 
         b clearInArr			# Keep looping
         
-        
-#
+
 # Exiting the program at this point.
-#
 exit:
 	# Print out prompt
     	la  	$a0, exit_prompt   	# Loads addr of prompt_input into $a0
