b5ee9575281abf350f8dc4ddc9c7bad1b9c43e85
Jason <jason_downing@student.uml.edu>
2014-11-01 21:20:11 -0400

    nearly done HW5

--- CIII/HW5/matrix.cpp
+++ CIII/HW5/matrix.cpp
@@ -20,6 +20,12 @@ class Matrix
         // Constructor, creates n by m matrix with elements on the diagonal = d, elsewhere = 0.
         Matrix(int _m, int _n, int d);
 
+        // Copy constructor
+        Matrix(const Matrix& copy);
+
+        // Destructor
+        ~Matrix();
+
         // Function which computes the determinant of a matrix. (EXTRA CREDIT)
         // int det();
 
@@ -36,8 +42,8 @@ class Matrix
     private:
 
         // Member variables
-        int n, m;                   // Dimensions of the matrix
-        int** e;                     // nxm array of integers, where n is equal to 3.
+        int m, n;                   // Dimensions of the matrix
+        int** e;                    // nxm array of integers, where n is equal to 3.
 };
 
 
@@ -54,6 +60,7 @@ Matrix::Matrix()
         return;
     }
 
+    // Allocate each array
     for(int i = 0; i < m; i++)
     {
         e[i] = new(nothrow) int[n];     // This will allocate each individual array.
@@ -66,9 +73,9 @@ Matrix::Matrix()
     }
 
     // Put 0's throughout the matrix.
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int x = 0; x < m; x++)
+        for(int x = 0; x < n; x++)
         {
             e[i][x] = 0;        // This double for loop should do the job nicely.
         }
@@ -79,8 +86,8 @@ Matrix::Matrix()
 // Default constructor, creates n by m matrix with elements = 0.
 Matrix::Matrix(int _m, int _n)
 {
-    n = _n;
     m = _m;
+    n = _n;
     e = new(nothrow) int*[m];       // Allocate the array of pointers.
 
     // Make sure the memory allocated! If it didn't, print an error.
@@ -101,9 +108,9 @@ Matrix::Matrix(int _m, int _n)
     }
 
     // Put 0's throughout the matrix.
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int x = 0; x < m; x++)
+        for(int x = 0; x < n; x++)
         {
             e[i][x] = 0;        // This double for loop should do the job nicely.
         }
@@ -114,8 +121,8 @@ Matrix::Matrix(int _m, int _n)
 // Constructor, creates n by m matrix with elements on the diagonal = d, elsewhere = 0.
 Matrix::Matrix(int _m, int _n, int d)
 {
-    n = _n;
     m = _m;
+    n = _n;
     e = new(nothrow) int*[m];       // Allocate the array of pointers.
 
     // Make sure the memory allocated! If it didn't, print an error.
@@ -136,9 +143,9 @@ Matrix::Matrix(int _m, int _n, int d)
     }
 
     // Put 0's throughout the matrix, except for the diagonals.
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < m; i++)
     {
-        for(int x = 0; x < m; x++)
+        for(int x = 0; x < n; x++)
         {
             if(i == x) 
             {   // When i equals x, it's a diagonal.
@@ -153,13 +160,64 @@ Matrix::Matrix(int _m, int _n, int d)
 }
 
 
+// Copy constructor
+Matrix::Matrix(const Matrix& copy)
+{
+    m = copy.m;
+    n = copy.n;
+    e = new(nothrow) int*[m];       // Allocate the array of pointers.
+
+    // Make sure the memory allocated! If it didn't, print an error.
+    if(e == NULL) {
+        cout << "Error: memory could not be allocated.\n";
+        return;
+    }
+
+    for(int i = 0; i < m; i++)
+    {
+        e[i] = new(nothrow) int[n];     // This will allocate each individual array.
+
+        // Make sure the memory allocated! If it didn't, print an error!
+        if(e[i] == NULL) {
+            cout << "Error: memory could not be allocated.\n";
+            return;
+        }
+    }
+
+    // Make this Matrix a complete copy of Matrix copy
+    for(int i = 0; i < m; i++)
+    {
+        for(int x = 0; x < n; x++)
+        {
+            e[i][x] = copy.e[i][x];
+        }
+    }
+}
+
+
+// Destructor
+Matrix::~Matrix()
+{
+    // Need to free every array in the 2D array.
+    // Then free the array of arrays.
+    
+    for(int i = 0; i < m; i++)
+    {
+            delete [] e[i];     // Delete the array within an array.
+    }
+
+    // Delete the array of arrays.
+    delete [] e;
+}
+
+
 // Input function from either a file or standard IO
 istream& operator >>(istream& in, Matrix& trix)
 {
     // Input the Matrix.
-    for(int i = 0; i < trix.n; i++)
+    for(int i = 0; i < trix.m; i++)
     {
-        for(int x = 0; x < trix.m; x++)
+        for(int x = 0; x < trix.n; x++)
         {
             in >> trix.e[i][x];       // >> will redirect the input to the 3x3 array.
         }
@@ -173,11 +231,11 @@ istream& operator >>(istream& in, Matrix& trix)
 ostream& operator <<(ostream& out, const Matrix& trix)
 {
     // Output the Matrix.
-    for(int i = 0; i < trix.n; i++)
+    for(int i = 0; i < trix.m; i++)
     {
         out << "    ";                    // Tabs make output look pretty.
 
-        for(int x = 0; x < trix.m; x++)
+        for(int x = 0; x < trix.n; x++)
         {
             out << setw(6) << right;      // Make the output look pretty.
             out << trix.e[i][x];          // << will redirect the output to the output stream.
@@ -198,7 +256,7 @@ bool operator ==(const Matrix& one, const Matrix& two)
 
     // First make sure the two matrixes are the same size.
     // n and m should be the same for one and two
-    if( (one.n != two.n) || (one.m != two.m) )
+    if( (one.m != two.m) || (one.n != two.n) )
     {
         // Either n wasn't the same, m wasn't the same 
         // or both were not the same.
@@ -207,9 +265,9 @@ bool operator ==(const Matrix& one, const Matrix& two)
         return false;
     }
 
-    for(int i = 0; i < one.n; i++)
+    for(int i = 0; i < one.m; i++)
     {
-        for(int x = 0; x < one.m; x++)
+        for(int x = 0; x < one.n; x++)
         {
             // If one spot is not equal, then the
             // two matrices are not equal to each other.
@@ -230,12 +288,12 @@ bool operator ==(const Matrix& one, const Matrix& two)
 // Copies a matrix
 Matrix& Matrix::operator=(const Matrix& trix)
 {
-    Matrix temp(trix.n, trix.m);        // Call the default construct with trix's dimensions.
+    Matrix temp(trix.m, trix.n);        // Call the default construct with trix's dimensions.
 
     // Go through and copy every int over to temp.
-    for(int i = 0; i < temp.n; i++)
+    for(int i = 0; i < temp.m; i++)
     {
-        for(int x = 0; x < temp.m; x++)
+        for(int x = 0; x < temp.n; x++)
         {
             // Copy the right matrix into the left matrix.
             temp.e[i][x] = trix.e[i][x];
@@ -252,7 +310,7 @@ Matrix operator +(const Matrix& one, const Matrix& two)
 {
     // First make sure the two matrixes are the same size.
     // n and m should be the same for one and two
-    if( (one.n != two.n) || (one.m != two.m) )
+    if( (one.m != two.m) || (one.n != two.n) )
     {
         // Either n wasn't the same, m wasn't the same 
         // or both were not the same.
@@ -261,11 +319,11 @@ Matrix operator +(const Matrix& one, const Matrix& two)
         return one;
     }
 
-    Matrix temp(one.n, one.m);        // Call the default construct with one's dimensions.
+    Matrix temp(one.m, one.n);        // Call the default construct with one's dimensions.
 
-    for (int i = 0; i < temp.n; i++)
+    for (int i = 0; i < temp.m; i++)
     {
-        for (int x = 0; x < temp.m; x++)
+        for (int x = 0; x < temp.n; x++)
         {
             // Add all the matrix spots into temp's matrix e.
             temp.e[i][x] = one.e[i][x] + two.e[i][x];
@@ -282,7 +340,7 @@ Matrix operator -(const Matrix& one, const Matrix& two)
 {
     // First make sure the two matrixes are the same size.
     // n and m should be the same for one and two
-    if( (one.n != two.n) || (one.m != two.m) )
+    if( (one.m != two.m) || (one.n != two.n) )
     {
         // Either n wasn't the same, m wasn't the same 
         // or both were not the same.
@@ -291,9 +349,9 @@ Matrix operator -(const Matrix& one, const Matrix& two)
         return one;
     }
 
-    Matrix temp(one.n, one.m);        // Call the default construct with one's dimensions.
+    Matrix temp(one.m, one.n);        // Call the default construct with one's dimensions.
 
-    for (int i = 0; i < temp.n; i++)
+    for (int i = 0; i < temp.m; i++)
     {
         for (int x = 0; x < temp.n; x++)
         {
@@ -311,9 +369,9 @@ Matrix operator -(const Matrix& one, const Matrix& two)
 Matrix operator -(const Matrix& only)
 {
     // Object to hold the addition.
-    Matrix temp(only.n, only.m);
+    Matrix temp(only.m, only.n);
 
-    for (int i = 0; i < temp.n; i++)
+    for (int i = 0; i < temp.m; i++)
     {
         for (int x = 0; x < temp.n; x++)
         {
@@ -334,11 +392,11 @@ Matrix operator *(const Matrix& one, const Matrix& two)
     Matrix temp;
 
     // Multiply 3x3 matrices. Using 3 loops will do it.
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < temp.m; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for(int j = 0; j < temp.n; j++)
         {
-            for (int k = 0; k < 3; k++)
+            for (int k = 0; k < temp.n; k++)
             {
                    temp.e[i][j] += one.e[i][k] * two.e[k][j];
             }
@@ -374,7 +432,81 @@ int main()
     Matrix Z3;      
 
     // Output it to the screen.
-    cout << Z3;
+    cout << "Matrix Z3: \n" << Z3 << endl;
+
+    // 3 by 2 matrix with all zeroes.
+    Matrix Z(3, 2);
+
+    // Output it to the screen.
+    cout << "Matrix Z: \n" << Z << endl;
+
+    // 3 x 2 matrix with elements inputted from file.
+    Matrix A(3, 2);
+    in_stream >> A;
+
+    // Output A.
+    cout << "Matrix A: \n" << A << endl;
+
+    // B is a copy of A.
+    Matrix B(A);
+
+    // Make sure B == A
+    cout << "B == A: ";
+    B == A;
+    cout << endl;
+
+    // C is a 3 x 2 matrix with elements inputted from file
+    Matrix C(3, 2);
+    in_stream >> C;
+
+    // output C
+    cout << "Matrix C: \n" << C << endl;
+
+    // E is a diagonal 2x2 matrix with 1 on the main diagonal and 0's everywhere else
+    Matrix E(2, 2, 1);
+
+    // output E
+    cout << "Matrix E: \n" << E << endl;
+
+    // D is a diagonal 2x2 matrix with 2 on the main diagonal and 0's everywhere else
+    Matrix D(2, 2, 2);
+
+    // output D
+    cout << "Matrix D: \n" << D << endl;
+
+    // This should be true.
+    cout << "A==B: ";
+    A==B;
+
+    // And this should be false.
+    cout << "A==C: ";
+    A==C;
+    
+    // True.
+    cout << "A-B==Z: ";
+    A-B==Z;
+
+    // True.
+    cout << "-A==Z-A: \n";
+    //–A==Z-A;
+
+    // True.
+    cout << "A+B==A*D: \n";
+    //A+B==A*D;
+
+    // True.
+    cout << "A*E==A: \n";
+    //A*E==A;
+
+    A=C;
+
+    // This should be false.
+    cout << "A==B: ";
+    A==B;
+
+    // And this should be true.
+    cout << "A==C: ";
+    A==C;
 
     // Finally done. ^^
     return 0;
