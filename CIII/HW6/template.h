29ade6968704f4dd83ce604284a9626d58a9c3da
Jason <jason_downing@student.uml.edu>
2014-11-07 17:28:40 -0500

    wow, i got something to kinda work. kinda like magic but whatever

--- CIII/HW6/template.h
+++ CIII/HW6/template.h
@@ -6,11 +6,43 @@
 
 using namespace std;
 
+
+// Class Student
+class Student
+{
+    public:
+        Student();
+        Student(string set_name, string set_grade, string set_HW);
+        string get_element(int position);
+        void set_element(int position, string value);
+
+    private:
+        string name;
+        string grade;
+        string homework;
+};
+
+
+// Class Winner
+class Winner
+{
+    public:
+
+
+    private:
+        int person;
+        int age;
+        int DOB;
+};
+
+
 // Template class, uses STL
 template <class T>
 class Trio
 {
     public:
+        //Trio(T student);
+        //Trio(const Winner& copy);
         Trio(T one, T two, T three);
         T get_element(int position);
         void set_element(int position, T value);
@@ -21,6 +53,16 @@ class Trio
         T third;
 };
 
+// Copies an object of class Student into the Trio class.
+// template <class T>
+// Trio<T>::Trio(T student)
+// {
+//     //first = copy.name;
+//     //second = copy.grade;
+//     //third = copy.homework;
+// }
+
+
 // Default constructor which takes 3 parameters.
 template <class T>
 Trio<T>::Trio(T one, T two, T three)
@@ -29,6 +71,7 @@ Trio<T>::Trio(T one, T two, T three)
     second = two;
     third = three;
 }
+
 
 // Get's an element at position 1 or 2 or 3
 template <class T>
@@ -50,9 +93,10 @@ T Trio<T>::get_element(int position)
     else
     {
         // Error, need to pick 1, 2 or 3.
-        cout << "Position must be either 1, 2, or 3! \n";
+        cout << "\nError: Position must be either 1, 2, or 3! \n";
     }
 }
+
 
 // Sets an element at a given position, and puts the value there.
 template <class T>
@@ -74,34 +118,68 @@ void Trio<T>::set_element(int position, T value)
     else
     {
         // Error, need to pick 1, 2 or 3.
-        cout << "Position must be either 1, 2, or 3! \n";
+        cout << "\nError: Position must be either 1, 2, or 3! \n";    }
+}
+
+
+Student::Student()
+{
+    // tell the compiler to stfu.
+}
+
+
+// Default constructor for Class Student. Similar to class Trio.
+Student::Student(string set_name, string set_grade, string set_HW)
+{
+    name = set_name;
+    grade = set_grade;
+    homework = set_HW;
+}
+
+
+string Student::get_element(int position)
+{
+    // Choose position
+    if(position == 1)
+    {
+        return name;                // Once we pick a position, just return whatever
+    }                                       // variable is at that position in the class.
+    else if(position == 2)
+    {
+        return grade;
+    }
+    else if(position == 3)
+    {
+        return homework;
+    }
+    else
+    {
+        // Error, need to pick 1, 2 or 3.
+        return "\nError: Position must be either 1, 2, or 3! \n";
     }
 }
 
-// Class Student
-class Student
+
+void Student::set_element(int position, string value)
 {
-    public:
-        Student(int one);
-        int get_element(int position);
-        void set_element(int position, int set);
-
-    private:
-        string name;
-        string 
-        int GPA;
-};
-
-// Class Winner
-class Winner
-{
-    public:
-
-
-    private:
-        string name;
-
-};
-
+    // Choose position
+    if(position == 1)
+    {
+        name = value;
+    }
+    else if(position == 2)
+    {
+        grade = value;
+    }
+    else if(position == 3)
+    {
+        homework = value;
+    }
+    else
+    {
+        // Error, need to pick 1, 2 or 3.
+        cout << "\nError: Position must be either 1, 2, or 3! \n";
+    }
+}
 
 #endif
--- CIII/HW6/templateTester.cpp
+++ CIII/HW6/templateTester.cpp
@@ -10,11 +10,15 @@ int main()
     Trio<int> xyz_coordinates(5, -5, 3);
     Trio<string> name_initials("JD", "JT", "HH");
     Trio<string> full_name("Jason Downing", "JT Shepple", "Huy Huyuh");
-    Trio<string> car("Ford", "Ranger", "2-fa$t");     
-    
+    Trio<string> car("Ford", "Ranger", "2-fa$t");
+
     // These use class objects
-    Trio<string> top_students("JT", "JT", "JT");
-    Trio<string> winners("Huy", "Jamil", "Jason");
+    Student JT("JT", "Sophomore", "Read Chap 5");
+    Student Jason("Jason", "Sophomore", "None");
+    Student Huy("Huy", "Sophomore", "None");
+
+    Trio<Student> top_students(JT, Jason, Huy);
+    //Trio<Student> winners("Huy", "Jamil", "Jason");
 
     cout << "Just recreated 6 objects. Testing them now: \n";
 
@@ -24,16 +28,30 @@ int main()
     full_name.set_element(3, "Udit Bdfjkfdsfdc");
     car.set_element(3, "2-sl0w");
 
-    top_students.set_element(1, "JD");
-    winners.set_element(2, "John");
+    // Changed Student Jason to Student JT.
+    top_students.set_element(2, JT);
+
+    // Changed Student Huy to Student Jason.
+    top_students.set_element(3, Jason);
+    //winners.set_element(2, "John");
 
     // Output the 6 objects.
     cout << "Y coordinate: " << xyz_coordinates.get_element(2) << "\n";
     cout << "1st Initial: " << name_initials.get_element(1) << "\n";
     cout << "3rd Full name: " << full_name.get_element(3) << "\n";
-    cout << "Car Model: " << car.get_element(2) << "\n";
-    cout << "Top student: " << top_students.get_element(1) << "\n";
-    cout << "Winner of the contest: " << winners.get_element(1) << "\n";
+    cout << "Car Model: " << car.get_element(2) << "\n\n";
+
+    // This is like hacking, or voodo magic, but apparently getting a class and then getting
+    // a string of that class and outputting it works.
+    cout << "Top student: " << top_students.get_element(1).get_element(1) << "\n";
+    cout << "His grade: " << top_students.get_element(1).get_element(2) << "\n";
+    cout << "Homework : " << top_students.get_element(1).get_element(3) << "\n";
+    cout << "This will not work : " << top_students.get_element(1).get_element(5) << "\n";
+
+    cout << "Runner up: " << top_students.get_element(2).get_element(1) << "\n";
+    cout << "Last student: " << top_students.get_element(3).get_element(1) << "\n";
+
+    //cout << "Winner of the contest: " << winners.get_element(1) << "\n";
 
     return 0;
 }
