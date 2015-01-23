afc743790e7714c76c2b2aa4aa0d9db0f700eb8e
Jason <jason_downing@student.uml.edu>
2015-01-23 01:11:23 -0500

    started playing around with sfml

--- CIV/P0/main.cpp
+++ CIV/P0/main.cpp
@@ -2,21 +2,43 @@
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+  sf::RenderWindow window(sf::VideoMode(500, 375), "SFML works!");
   sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+
+  // Loads a sprite from file - in this case a doge
+  sf::Texture texture;
+  if (!texture.loadFromFile("doge.jpg"))
+    return EXIT_FAILURE;
+  sf::Sprite sprite(texture);
+
+  // Creates text to display on the screen.
+  sf::Font font;
+  if (!font.loadFromFile("arial.ttf"))
+
+  // Make some text to display
+  sf::Text text("Hello SFML", font, 50);
 
   while (window.isOpen())
   {
+    // Process events
     sf::Event event;
     while (window.pollEvent(event))
     {
+      // Close window : exit
       if (event.type == sf::Event::Closed)
         window.close();
     }
 
+    // Clear screen
     window.clear();
-    window.draw(shape);
+
+    // Draw the sprite
+    window.draw(sprite);
+
+    // Draw the string
+    window.draw(text);
+
+    // Update the window
     window.display();
   }
 
