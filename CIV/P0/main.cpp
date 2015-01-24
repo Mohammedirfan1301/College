ad0d5349d4e1ae320f048fe51b8809bdf9ed3300
Jason <jason_downing@student.uml.edu>
2015-01-24 00:08:07 -0500

    nearly done p0

--- CIV/P0/main.cpp
+++ CIV/P0/main.cpp
@@ -1,39 +1,124 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <string>
+
+using namespace std;
+using namespace sf;
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(500, 375), "SFML works!");
-  sf::CircleShape shape(100.f);
+  // Set the size of the window.
+  // In this case I make it 605 by 605. The title is "Program 0".
+  sf::RenderWindow window(sf::VideoMode(605, 605), "Program 0: Doge Meme");
 
-  // Loads a sprite from file - in this case a doge
-  sf::Texture texture;
-  if (!texture.loadFromFile("doge.jpg"))
+  // Loads a doge from file - in this case a doge
+  sf::Texture image_texture;
+
+  // Check to see if the file loaded correctly.
+  // I make just the dog part of the image load - it's actually 300 by 372
+  // but I cut off 50 pixels from the height.
+  if (!image_texture.loadFromFile( "doge.png",  sf::IntRect(0, 50, 300, 300) ) )
+  {
+    // Return failure if the image doesn't load
+    cout << "Failed to load image!\n";
     return EXIT_FAILURE;
-  sf::Sprite sprite(texture);
+  }
 
-  // Creates text to display on the screen.
+  // Make the image look nice.
+  image_texture.setSmooth(true);
+
+  // Now set the doge using the texture
+  sf::Sprite doge;
+
+  doge.setTexture(image_texture);
+
+  // Set the origin for rotation to the center of the doge.
+  doge.setOrigin(150, 125);
+
+  doge.move(302, 200);
+
+  // Create a graphical text to display
   sf::Font font;
   if (!font.loadFromFile("arial.ttf"))
+  {
+    cout << "Failed to load font!\n";
+    return EXIT_FAILURE;
+  }
+  sf::Text text("Woof", font, 70);
 
-  // Make some text to display
-  sf::Text text("Hello SFML", font, 50);
+  // Move the text to the bottom center (roughly)
+  text.move(230,500);
 
+  // Window loop
   while (window.isOpen())
   {
     // Process events
     sf::Event event;
-    while (window.pollEvent(event))
+
+    // See if the window closes
+    while(window.pollEvent(event))
     {
       // Close window : exit
-      if (event.type == sf::Event::Closed)
+      if (sf::Event::Closed)
+      {
         window.close();
+      }
+
+      // Move the image if an arrow key is pressed.
+      else if(sf::Event::KeyPressed)
+      {
+        // Arrow keys will move the doge in the
+        // expected direction.
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+          doge.move(-15, 0);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+          doge.move(15, 0);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+          doge.move(0, -15);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+          doge.move(0, 15);
+        }
+
+        // Enter will rotate the doge
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        {
+          doge.rotate(45);
+        }
+
+        // Pressing + will increase the size of the doge.
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add))
+        {
+          doge.scale(1.05f, 1.05f);
+        }
+
+        // Pressing - will decrease the size of the doge.
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract))
+        {
+          doge.scale(.95f, .95f);
+        }
+
+        // Pressing escape will quit the program.
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+          return 0;
+        }
+      }
+
     }
 
-    // Clear screen
+    // Clear the screen - so that the doge's previous image
+    // gets erased.
     window.clear();
 
-    // Draw the sprite
-    window.draw(sprite);
+    // Redraw the doge
+    window.draw(doge);
 
     // Draw the string
     window.draw(text);
