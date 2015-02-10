6140a7baa8b8a2f5b104425110ed4ab73308b1d9
Jason <jason_downing@student.uml.edu>
2015-02-09 19:30:26 -0500

    step is done I think...

--- CIV/ps2/LFSR.cpp
+++ CIV/ps2/LFSR.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "LFSR.hpp"
 
 // Implemenation file for the LFSR class
@@ -18,8 +20,43 @@ LFSR::LFSR(std::string seed, int t)
 // simulates one step
 int LFSR::step()
 {
-  std::cout << "One step.\n";
-  return 1;
+  /*
+   * We need to XOR the left most position with the tap position.
+   * The left most position is just bits[0] - the very left most bit.
+   *
+   * The tap position can be found using the following formula:
+   * tap position = size() - tap - 1
+   *
+   * It works by converting the tap position value into array indexes
+   *
+   */
+
+  // Find the index of the tap position.
+  int tap_pos = bits.length() - tap - 1;
+
+  // XOR the tap position with the left most bit
+  int res = bits[0] ^ bits[tap_pos];
+
+  // We need to go through and shift all bits left now.
+  std::string::size_type i;
+  std::ostringstream ostring;   // Using string streams to append the
+                                // the XOR result
+
+  // Now shift everything left.
+  for(i = 0; (unsigned)i < bits.length() - 1; i++)
+  {
+    ostring << bits[i + 1];
+  }
+
+  // Append the XOR result
+  ostring << res;
+
+  // Save the updated string - ostring.str() converts a string stream
+  // back into a string
+  bits = ostring.str();
+
+  // Return the XOR result bit
+  return res;
 }
 
 
--- CIV/ps2/main.cpp
+++ CIV/ps2/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "LFSR.hpp"
 
 // Testing the LFSR class
@@ -9,13 +11,57 @@ int main()
 
   std::cout << test << std::endl;
 
-  test.step();
+  std::string test_string("01101000010");
 
-  test.generate(10);
+  // Hope this works. Step once.
+  test.step();
 
   std::cout << test << std::endl;
 
-  std::cout << "Main is done!\n";
+// //   for(int x = 0; (unsigned)x < test_string.size(); x++)
+// //   {
+// //     std::cout << "Position " << x << " is: " << test_string[x] << std::endl;
+// //   }
+//
+//   // figure out the position of the tap using the magic formula:
+//   // position = size() - tap - 1
+//   int tap_pos = test_string.size() - 8 - 1;
+//
+// //   std::cout << test_string[0] << test_string[tap_pos] << std::endl;
+//
+//   int res = test_string[0] ^ test_string[tap_pos];
+//
+//   std::cout << "res: " << res << std::endl;
+//
+//   std::string::size_type i;
+//
+//   std::ostringstream ostring;
+//
+//   // Now shift everything left.
+//   for(i = 0; (unsigned)i < test_string.length() - 1; i++)
+//   {
+//     ostring << test_string[i + 1];
+//   }
+//
+//   ostring << res;
+//
+//   std::ostringstream oss;
+//   oss << test_string << res;
+//   std::cout << "final string should be: " << ostring.str() << std::endl;
+//
+//   test_string[i] = 1;
+//   std::cout << test_string[i] << "i = " << i << std::endl;
+//
+//   std::cout << "test size is: " << test_string.length() << std::endl;
+//   std::cout << test_string << " " << res << "\n";
+
+//   test.step();
+//
+//   test.generate(10);
+//
+//   std::cout << test << std::endl;
+//
+//   std::cout << "Main is done!\n";
 
   return 0;
 }
