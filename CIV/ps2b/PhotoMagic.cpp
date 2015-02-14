c5d53d38e73d007d08b2f46a0064a81a2ca854fd
Jason <jason_downing@student.uml.edu>
2015-02-13 23:07:01 -0500

    ps2b is pretty mch done.

---
+++ CIV/ps2b/PhotoMagic.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "LFSR.hpp"
+
+
+/*
+ *
+ *
+ */
+
+
+int main(int argc, char* argv[])
+{
+  // Must have
+  if(argc != 5)
+  {
+    std::cout << "Usage: $ ./PhotoMagic input-file.png output-file.png 01101000010100010000 16 \n";
+    return -1;
+  }
+
+  // Save the command line arguments to variables
+  std::string input_filename(argv[1]);
+  std::string output_filename(argv[2]);
+  std::string seed(argv[3]);
+  int tap = atoi(argv[4]);
+
+  // LSFR stuff
+  LFSR randomizer(seed, tap);
+
+  // Setup the two images
+  sf::Image input_image;
+  if (!input_image.loadFromFile(input_filename))
+  {
+    return -1;
+  }
+
+  sf::Image output_image;
+  if (!output_image.loadFromFile(input_filename))
+  {
+    return -1;
+  }
+
+  // p is a pixel
+  sf::Color p;
+
+  // Randomize the bits in the image
+  for (int x= 0; x < 300; x++)
+  {
+    for (int y = 0; y < 372; y++)
+    {
+      // Get the current pixel from the input image
+      p = input_image.getPixel(x, y);
+
+      // Change these to XOR
+      p.r = p.r ^ randomizer.generate(8);
+      p.g = p.g ^ randomizer.generate(8);
+      p.b = p.b ^ randomizer.generate(8);
+
+      // Modify just the output image
+      output_image.setPixel(x, y, p);
+    }
+  }
+
+  // Setup the two windows
+  sf::Vector2u size = input_image.getSize();
+  sf::RenderWindow input_window(sf::VideoMode(size.x, size.y), "Input Image");
+  sf::RenderWindow output_window(sf::VideoMode(size.x, size.y), "Output Image");
+
+  // Load the images into textures
+  sf::Texture input_texture, output_texture;
+  input_texture.loadFromImage(input_image);
+  output_texture.loadFromImage(output_image);
+
+  // Then load the textures into sprites
+  sf::Sprite input_sprite, output_sprite;
+  input_sprite.setTexture(input_texture);
+  output_sprite.setTexture(output_texture);
+
+  // Window loop
+  while (input_window.isOpen() && output_window.isOpen())
+  {
+    sf::Event event;
+
+    while (input_window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        input_window.close();
+      }
+    }
+
+    while (output_window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        output_window.close();
+      }
+    }
+
+    input_window.clear();
+    input_window.draw(input_sprite);    // Input image
+    input_window.display();
+
+    output_window.clear();
+    output_window.draw(output_sprite);    // Output image
+    output_window.display();
+  }
+
+  // Save the modified image to the output file
+  if (!output_image.saveToFile(output_filename))
+  {
+    return -1;
+  }
+
+  return 0;
+}
