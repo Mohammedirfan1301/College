5facf6a63acc57263853b7af7650762a43f8994b
Jason Downing <jason_downing@student.uml.edu>
2016-02-16 14:00:44 -0500

    adding ps3a for more testing.

--- OPL/ps3a/ps3a/ps3a.rkt
+++ OPL/ps3a/ps3a/ps3a.rkt
@@ -26,33 +26,37 @@
         (cons 2
               (cons 3 nil))))
 
-(define list2                        ;; (1 (2 3) 4)
+(define list2   ;; Should look like this in Racket: (1 (2 3) 4)
   (cons 1                            ;; (1
         (cons (cons 2 (cons 3 nil))  ;; '(2 3)
               (cons 4 nil))))        ;;  4)
                     
 
-(define list3                        ;; ((1 2) (3 4))
+(define list3   ;; Should look like this in Racket: ((1 2) (3 4))
   (cons (cons 1 (cons 2 nil)) ;; '(1 2)
               (cons (cons 3 (cons 4 nil)) nil)))  ;; '(3 4)
 
-(define list4                        ;; ((1 2))
+(define list4   ;; Should look like this in Racket: ((1 2))
   (cons (cons 1 (cons 2 nil)) nil))
 
 ;; now, define statements to produce these lists using
 ;; the quoting function (equivalent to their printed form).
 ;; the solution for list1 is provided as an exemplar
+;; This should be (1 2 3) in Racket.
 (define listq1
   '(1 2 3))
 
+;; Should look like this in Racket: (1 (2 3) 4)
 (define listq2
-  '())
+  '(1 '(2 3) 4))
 
+;; Should look like this in Racket: ((1 2) (3 4))
 (define listq3
-  '())
+  '('(1 2) '(3 4)))
 
+;; Should look like this in Racket: ((1 2))
 (define listq4
-  '())
+  '('(1 2)))
 
 ;; create box-and-pointer diagrams for the following list5 through list8.
 ;; you may create ASCII art and submit as comments,
@@ -64,31 +68,66 @@
 (define list5
   (cons 1 (list 2 3)))
 
-(define list5flag #f)
+;  ASCII Art below. → / ↓ are pointers to lists / cons.
+;
+;  [1][→]  [↓][/]
+;          [2][→]  [3][/]
+;
+
+(define list5flag #t)
 
 (define list6
   (cons 3 (cons 4 5)))
 
-(define list6flag #f)
+;  ASCII Art below. → / ↓ are pointers to lists / cons.
+;
+;  [3][→] [4][→] [5][/]
+;
+
+(define list6flag #t)
 
 (define list7
   (list 3 (cons 4 (list 5 6))))
 
-(define list7flag #f)
+;  ASCII Art below. → / ↓ are pointers to lists / cons.
+;
+;  [3][→]  [↓][/]
+;          [4][→]  [↓][/]
+;                  [5][→]  [6][/]
+;
+
+(define list7flag #t)
 
 (define list8
   (list (cons 3 4) (list 5 6)))
 
-(define list8flag #f)
+;  ASCII Art below. → / ↓ are pointers to lists / cons.
+;
+;  [↓][→]      [↓][/]
+;  [3][↓]      [5][→]  [6][/]
+;     [4][/]
+
+(define list8flag #t)
 
 ;; write procedures to sum up a list of numbers
 ;; first do it with recursive process, then iterative process
 ;; the base case must be that the sum of an empty list is 0.
 (define (sum-list-r lst)
-  1)
+  (if (eq? lst '())
+      0  ;; Base case of 0 + whatever
+      (+ (car lst)
+         (sum-list-r (cdr lst)))))
 
 (define (sum-list-i lst)
-  1)
+  (define (iter-helper sum lst) ;; Iterative helper function
+    (if (eq? lst '())  ;; '() is basically nil / null
+        sum    ;; Base case
+        (iter-helper (+ (car lst) sum)  ;; Feed in the sum
+                     (cdr lst))         ;; Keep moving down the list.
+    )
+  ) ;; Seed iter-helper with 0 for sum and the top of the list.
+  (iter-helper 0 lst)
+)
 
 
 ;; Consider the following recursive procedure:
