260e0d2a2275b71d8019ee83062475e59082c1c8
Jason Downing <jdown1994@gmail.com>
2016-03-05 16:21:33 -0500

    passing all 2.58 tests, woot.

--- OPL/ps4/ps4/exercise2-58.rkt
+++ OPL/ps4/ps4/exercise2-58.rkt
@@ -22,68 +22,88 @@
 (define (=number? exp num)
   (and (number? exp) (= exp num)))
 
+
+;; This should print out: "(x + 3)"
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
-	((=number? a2 0) a1)
-	((and (number? a1) (number? a2)) (+ a1 a2))
-	(else (list '+ a1 a2))))
+	    ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        ;; Print out like this instead: '(x + 3)
+	    (else (list a1 '+ a2))))  
 
+;; This should return "+" in the test file.
 (define (sum? x)
-  (and (pair? x) (eq? (car x) '+)))
+  ;; Make it "cadr x" to get the "+"!
+  (and (pair? x) (eq? (cadr x) '+)))
 
-(define (addend s) (cadr s))
+;; This should return "x" in the test file.
+(define (addend s) (car s))
 
+;; This should return "3" in the test file.
 (define (augend s) (caddr s))
 
+
+;; This should be modified to print out: '(x * 3)")
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-	((=number? m1 1) m2)
-	((=number? m2 1) m1)
-	((and (number? m1) (number? m2)) (* m1 m2))
-	(else (list '* m1 m2))))
+	    ((=number? m1 1) m2)
+	    ((=number? m2 1) m1)
+	    ((and (number? m1) (number? m2)) (* m1 m2))
+        ;; Print out like this instead: '(x * 3)")
+	    (else (list m1 '* m2))))
 
-(define (product? x) (and (pair? x) (eq? (car x) '*)))
+;; This should print out "#t" for the test case.
+(define (product? x) 
+  ;; Similar to sum?, we want "cadr" to get to the "*".
+  (and (pair? x) (eq? (cadr x) '*)))
 
-(define (multiplier p) (cadr p))
+;; This should print out "x" for the test file.
+(define (multiplier p) (car p))
 
+;; This should return "3" for the test file.
 (define (multiplicand p) (caddr p))
 
 
 ;;; differentiation for exponents
 
+;; This should print out: '(x ** 3)
 (define (make-exponentiation x y)
   (cond ((= y 0) 1)
-	((= y 1) x)
-	(else (list '** x y))))
+	     ((= y 1) x)
+        ;; Print out like this instead: '(x ** 3)
+	     (else (list x '** y))))
 
+;; This should print out "#t" for the test case.
 (define (exponentiation? x)
-  (and (pair? x) (eq? (car x) '**)))
+  ;; Similar to sum? / product?, we want "cadr" to get to the "**".
+  (and (pair? x) (eq? (cadr x) '**)))
 
-(define (base x) (cadr x))
+;; this should print out "'x" for the test file.
+(define (base x) (car x))
 
+;; this should print out "3" for the test file.
 (define (exponent x) (caddr x))
 
 ;;; deriv including exponentiation
 
 (define (deriv exp var)
   (cond ((number? exp) 0)
-	((variable? exp)
-	 (if (same-variable? exp var) 1 0))
-	((sum? exp)
-	 (make-sum (deriv (addend exp) var)
-		   (deriv (augend exp) var)))
-	((product? exp)
-	 (make-sum 
-	  (make-product (multiplier exp)
-			(deriv (multiplicand exp) var))
-	  (make-product (deriv (multiplier exp) var)
-			(multiplicand exp))))
-	((exponentiation? exp)
-	 (make-product (exponent exp)
-		       (make-product (make-exponentiation (base exp) 
-							  (- (exponent exp) 1))
-				     (deriv (base exp) var))))
-	(else
-	 (error "unknown expression type -- DERIV" exp))))
+	    ((variable? exp)
+	       (if (same-variable? exp var) 1 0))
+	    ((sum? exp)
+	       (make-sum (deriv (addend exp) var)
+		         (deriv (augend exp) var)))
+	    ((product? exp)
+	       (make-sum (make-product (multiplier exp)
+			               (deriv (multiplicand exp) var))
+	                 (make-product (deriv (multiplier exp) var)
+			               (multiplicand exp))))
+	    ((exponentiation? exp)
+	       (make-product (exponent exp)
+		             (make-product (make-exponentiation (base exp) 
+							        (- (exponent exp) 1))
+				           (deriv (base exp) var))))
+	    (else
+	       (error "unknown expression type -- DERIV" exp))))
 
 
