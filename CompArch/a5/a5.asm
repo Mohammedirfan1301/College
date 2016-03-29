ebddb07ffe8330adbb385e695b750ca9586e2ebe
Jason Downing <jdown1994@gmail.com>
2016-03-29 18:29:46 -0400

    more work on a5

--- CompArch/a5/a5.asm
+++ CompArch/a5/a5.asm
@@ -1,60 +1,35 @@
+; Jason Downing
+; Assignment 5
+; Used http://www.cs.uml.edu/~bill/cs305/IO_str_and_echo.asm for help.
+
 AddInput:             ; Main loop.
   lodd start:         ; Starting value
-  stod 4095           ; load and start UART
+  stod 4095           ; Start transmitter
   call xbsywt:        ; Receiver function call.
   loco rangeStr:      ; Load "Enter an int" string
-  call reader:        ; Print out string
+  call nextw:         ; Print out string
   call input:         ; Get input
-  lodd bin:           ; ???
+  lodd bin:           ; Load binary number
   stod sum:           ; Save the sum
   loco rangeStr:      ; Load "Enter an int" string
-  call reader:        ; Print out string
+  call nextw:         ; Print out string
   call input:         ; Get input
-  lodd bin:           ; ???
+  lodd bin:           ; Load binary number
   addd sum:           ; Add the sum.
-  stod sum:           ; Store it
-  stod data:          ; ???
+  stod sum:           ; Store
+  stod data:          ; Store
   jneg overflow:      ; Overflow error.
   loco sumStr:        ; Load summary string
-  call reader:        ; Print summary string
+  call nextw:         ; Print summary string
   call encode:        ; ???
   lodd zero:          ; returns 0
   halt
 
-overflow:             ; Output overflow string.
-  loco errorStr:
-  call reader:
-  lodd negone:        ; returns -1
+overflow:             ; overflow function
+  loco errorStr:      ; load overflow error string.
+  call nextw:         ; output to the screen
+  lodd negone:        ; put -1 into the AC
   halt
-
-reader:               ; Outputs a string that has been loaded.
-  pshi
-  addd one:
-  stod str:
-  pop
-  jzer crnl:          ; carriage return / newline function
-  stod 4094
-  push
-  subd c255:
-  jneg crnl:          ; carriage return / newline function
-  call sb:            ; does something important.
-  insp 1
-  push
-  call xbsywt:
-  pop
-  stod 4094
-  call xbsywt:
-  lodd str:
-  jump reader:
-
-crnl:                 ; This function outputs a carriage return and newline.
-  lodd cr:            ; carriage return
-  stod 4094           ; 4094 is where the CR is written
-  call xbsywt:        ; output carriage return
-  lodd nl:            ; newline
-  stod 4094           ; 4094 is where the NR is written
-  call xbsywt:        ; output newline
-  retn
 
 input:                ; Get input?
   lodd start:
@@ -82,31 +57,6 @@ endnum:               ; Last number?
   stod bin:
   lodd 4092
   loco 0
-  retn
-
-sb:                   ; this appears to be important.
-  loco 8
-
-case:                 ; Check to see if we're done.
-  jzer finish:
-  subd one:
-  stod count:
-  lodl 1
-  jneg increase:
-  addl 1
-  stol 1
-  lodd count:
-  jump case:
-
-increase:             ; increase counters.
-  addl 1
-  addd one:
-  stol 1
-  lodd count:
-  jump case:
-
-finish:               ; DONE FINALLY.
-  lodl 1
   retn
 
 encode:               ; encode and loop for all chars
@@ -140,36 +90,88 @@ output:               ; Output a number.
   call xbsywt:
   jump output:
 
-xbsywt:               ; Transmitter (output loop)
-  lodd 4095
-  subd mask:
-  jneg xbsywt:
-  retn
+; *******************************************************
+; This code is from the help file location here:
+; http://www.cs.uml.edu/~bill/cs305/IO_str_and_echo.asm
+; *******************************************************
+nextw:      pshi                ; Outputs a string that has been loaded.
+            addd one:
+            stod str:
+            pop
+            jzer crnl:          ; carriage return / newline function
+            stod 4094
+            push
+            subd c255:
+            jneg crnl:          ; carriage return / newline function
+            call sb:            ; calls the sb function to loco 8
+            insp 1
+            push
+            call xbsywt:
+            pop
+            stod 4094
+            call xbsywt:
+            lodd str:
+            jump nextw:
 
-rbsywt:               ; Receiver (input loop)
-  lodd 4093
-  subd mask:
-  jneg rbsywt:
-  retn
+; This function outputs a carriage return and newline.
+crnl:       lodd cr:            ; carriage return
+            stod 4094           ; 4094 is where the CR is written
+            call xbsywt:        ; output carriage return
+            lodd nl:            ; newline
+            stod 4094           ; 4094 is where the NR is written
+            call xbsywt:        ; output newline
+            retn
 
-done:      retn       ; Return function
+xbsywt:     lodd 4095           ; Transmitter (output loop)
+            subd mask:
+            jneg xbsywt:
+            retn
+
+rbsywt:     lodd 4093           ; Receiver (input loop)
+            subd mask:
+            jneg rbsywt:
+            retn
+
+sb:         loco 8              ; SB function
+
+loop1:      jzer finish:        ; Check to see if we're done.
+            subd one:
+            stod count:
+            lodl 1
+            jneg add1:
+            addl 1
+            stol 1
+            lodd count:
+            jump loop1:
+
+add1:       addl 1              ; Add 1 to counters
+            addd one:
+            stol 1
+            lodd count:
+            jump loop1:
+
+finish:     lodl 1              ; finished so return.
+            retn
+
+; Not sure if this is needed.
+done:       retn        ; Return function
 
 ; Variables down here.
 next:        0
-bin:         0
-sum:         0
+bin:         0        ;; Binary number
+sum:         0        ;; Sum variable
 data:        0
 count:       0
 str:         0
-mask:       10
-start:       8
+mask:       10        ;; Mask
+start:       8        ;; Start transmitter
 nl:         10        ;; Newline
 cr:         13        ;; Carriage Return
 negone:     -1        ;; constant -1
 zero:        0        ;; constant  0
 one:         1        ;; constant  1
-numset:     48
-c255:      255
+numset:     48        ;; constant 48
+c255:      255        ;; constant 255
 rangeStr:  "Enter an integer between 1 and 32767: "
 sumStr:    "The sum of these numbers is:"
 errorStr:  "Overflow, no sum possible!"
