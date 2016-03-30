58459369e7131659708a45422f8b62e9600e72f8
Jason Downing <jdown1994@gmail.com>
2016-03-29 22:09:58 -0400

    nearly done

--- CompArch/a5/a5.asm
+++ CompArch/a5/a5.asm
@@ -5,25 +5,25 @@
 ; *******************************************************
 ;  Main loop and functions
 ; *******************************************************
+
 AddInput:   lodd on:                ; Starting value of 8.
             stod 4095               ; Transmitter address
             call xbsywt:            ; Receiver function call.
 
-            ; First number
+            ; First number input
             loco rangeStr:          ; Load "Enter an int" string
             call nextw:             ; Print out string
             call getInput:          ; Get input from the user
             lodd binarynum:         ; Load binary number
             stod sum:               ; Save the sum
 
-            ; Second number
+            ; Second number input
             loco rangeStr:          ; Load "Enter an int" string
             call nextw:             ; Print out string
             call getInput:          ; Get input
             lodd binarynum:         ; Load binary number
             addd sum:               ; Add the sum.
-            stod sum:               ; Store sum
-            stod data:              ; Store sum into data as well.
+            stod sum:               ; Store the sum
 
             ; If sum went negative, we've overflowed.
             jneg OverFlow:          ; Overflow error.
@@ -40,13 +40,13 @@ AddInput:   lodd on:                ; Starting value of 8.
 getInput:   lodd on:                ; Transmitter on
             stod 4093               ; Address
             call rbsywt:            ; Call transmitter
-            lodd 4092               ; Get first digit
-            subd ascii:             ; Subtract 48
+            lodd 4092               ; Get first digit from input buffer
+            subd ascii:             ; Subtract 48 (ASCII value of 0)
             push                    ; Push to stack
 
 ; Get Digits loop
 getDigit:   call rbsywt:            ; Call transmitter
-            lodd 4092               ; get next digit
+            lodd 4092               ; get next digit from input buffer
             stod nextChar:          ; store it in next
             subd nl:                ; Subtract 10 to see if the NL char was entered.
             jzer lastNum:           ; NL char was entered, so done!
@@ -60,44 +60,45 @@ getDigit:   call rbsywt:            ; Call transmitter
 ; We're done, so save the number.
 lastNum:    pop                     ; Get the number
             stod binarynum:         ; Store it in the binary number var
-            lodd 4092               ; Load the receiver
+            lodd 4092               ; Load the input buffer
             loco 0
             retn                    ; Done so return.
 
 ; Overflow function
 OverFlow:   loco errorStr:          ; load overflow error string.
             call nextw:             ; output to the screen
-            lodd negone:            ; put -1 into the AC
+            lodd neg1:              ; put -1 into the AC
             halt                    ; HALT DON'T CATCH FIRE
 
 ; Convert function
 convert:    lodd on:                ; Start transmitter
             stod 4095               ; Transmitter address
-            lodd negone:            ; load -1 into AC
+            lodd neg1:              ; load -1 into AC
             push                    ; push -1 onto stack
             lodd mask:              ; load mask into AC
             push                    ; push mask onto stack
+            jump answer:            ; Jump to answer
 
 ; Loop for getting the answer
-answer:     lodd data:              ; Load answer into AC
+answer:     lodd sum:               ; Load answer into AC
             jzer output:            ; When done, print the answer.
             lodd mask:              ; Load mask
             push                    ; Push mask onto stack
-            lodd data:              ; Load answer into AC
+            lodd sum:               ; Load answer into AC
             push                    ; Push answer onto stack
             div                     ; Divide the answer by the mask
             pop                     ; Get result into AC
-            stod data:              ; Store new value into answer
+            stod sum:               ; Store new value into answer
             pop                     ; Pop stack
             insp 2                  ; Clean up the stack
-            addd ascii:             ; Convert to character.
+            addd ascii:             ; Convert to character by adding ASCII value of 0.
             push                    ; Push the AC onto stack
             jump answer:            ; Keep looping
 
 ; Output result function.
 output:     pop                     ; Get result into AC
             jneg done:              ; if -1 we're done!
-            stod 4094               ; store AC into register
+            stod 4094               ; store AC into output buffer
             call xbsywt:            ; print result
             jump output:            ; print the next result.
 
@@ -110,7 +111,7 @@ nextw:      pshi                    ; Outputs a string that has been loaded.
             stod str:
             pop
             jzer crnl:              ; carriage return / newline function
-            stod 4094
+            stod 4094               ; output buffer
             push
             subd c255:
             jneg crnl:              ; carriage return / newline function
@@ -119,27 +120,27 @@ nextw:      pshi                    ; Outputs a string that has been loaded.
             push
             call xbsywt:
             pop
-            stod 4094
+            stod 4094               ; output buffer
             call xbsywt:
             lodd str:
             jump nextw:
 
 ; This function outputs a carriage return and newline.
 crnl:       lodd cr:                ; carriage return
-            stod 4094               ; 4094 is where the CR is written
+            stod 4094               ; write CR to output buffer
             call xbsywt:            ; output carriage return
             lodd nl:                ; newline
-            stod 4094               ; 4094 is where the NR is written
+            stod 4094               ; write NL to output buffer
             call xbsywt:            ; output newline
             retn
 
 ; These are the transmitter / receiver functions for I/O
-xbsywt:     lodd 4095               ; Transmitter (output loop)
+xbsywt:     lodd 4095               ; Transmitter address (output loop)
             subd mask:
             jneg xbsywt:
             retn
 
-rbsywt:     lodd 4093               ; Receiver (input loop)
+rbsywt:     lodd 4093               ; Receiver address (input loop)
             subd mask:
             jneg rbsywt:
             retn
@@ -171,20 +172,26 @@ done:       retn                    ; Return function
 ; *******************************************************
 ;   Variables
 ; *******************************************************
+
+; NOTE:
+; 4092 = input buffer
+; 4093 = receiver address
+; 4094 = output buffer
+; 4095 = transmitter address
+
 nextChar:    0        ;; next char
 binarynum:   0        ;; binary number
 sum:         0        ;; Sum variable
-data:        0        ;; Data variable
 count:       0        ;; Counter
 str:         0        ;; String ptr
 mask:       10        ;; Mask
 on:          8        ;; Start transmitter
-nl:         10        ;; Newline
-cr:         13        ;; Carriage Return
-negone:     -1        ;; constant -1
+nl:         10        ;; ASCII value of Newline (nl)
+cr:         13        ;; ASCII value of Carriage Return (cr)
+neg1:       -1        ;; constant -1
 zero:        0        ;; constant  0
 one:         1        ;; constant  1
-ascii:      48        ;; constant 48 (ASCII 0)
+ascii:      48        ;; constant 48 (ASCII value for 0)
 c255:      255        ;; constant 255
 rangeStr:  "Enter an integer between 1 and 32767: "   ;; Output strings
 sumStr:    "The sum of these numbers is:"
