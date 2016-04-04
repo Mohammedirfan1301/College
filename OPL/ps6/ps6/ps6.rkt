cc863395c378c76ab71b2a56e7831ec7a4677ae3
Jason Downing <jdown1994@gmail.com>
2016-04-03 21:14:49 -0400

    nearly done...

--- OPL/ps6/ps6/ps6.rkt
+++ OPL/ps6/ps6/ps6.rkt
@@ -10,6 +10,7 @@
 ;; exercise3.1-env.png (or .jpg)
 
 (define (make-accumulator sum)
+  ; Simple accumulator.
   (lambda (x)
     (set! sum (+ sum x))
     sum
@@ -29,11 +30,12 @@
 ;; exercise3.2-env.png (or .jpg)
 
 (define (make-monitored f)
-  (define calls 0)
-  (define (reset_calls) 
+  (define calls 0)        ; Number of times the monitor has been called.
+  (define (reset_calls)   ; Resets the number of calls variable.
     (set! calls 0))
   (define (mf sym)
-    (cond   ;; Reset symbol is the input
+    (cond   
+      ;; Reset symbol is the input
       ((eq? sym 'reset) reset_calls)
       
       ;; How many calls symbol is the input
@@ -51,6 +53,7 @@
 ;; Exercise 3.3
 ;; https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-20.html#%_thm_3.3
 (define (make-account balance password)
+  ; From the SICP book
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance (- balance amount))
@@ -61,10 +64,10 @@
     balance)
   
   ; Displays an incorrect password error, like the test file wants.
-  (define (display-incorrect useless)
+  (define (display-incorrect useless)  ; the useless parameter is to stop errors.
     "Incorrect password")
   
-  ; Modified this function.
+   ; Modified this function from the SICP book.
   (define (dispatch pass sym)
     (if (eq? pass password)
         (cond ((eq? sym 'withdraw) withdraw)
@@ -82,6 +85,7 @@
 ;;   once an account is locked, you should not be able to withdraw funds
 ;;   from it, even if then using the correct password!
 (define (make-account-secure balance password)
+  ; From the SICP book
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance (- balance amount))
@@ -92,10 +96,10 @@
     balance)
   
   ; Displays an incorrect password error, like the test file wants.
-  (define (display-incorrect useless)
+  (define (display-incorrect useless)   ; the useless parameter is to stop errors.
     "Incorrect password")
   
-  ; Modified this function.
+  ; Modified this function from the SICP book.
   (define password-attempts 0)
   (define (dispatch pass sym)
     (if (eq? pass password)
@@ -112,14 +116,16 @@
         
         ; Increase password attempts and see if we should 
         ; call the cops or display an error.
-        (begin (set! password-attempts
-                     (+ password-attempts 1))
-               (if (> password-attempts 7)
+        (begin (set! password-attempts         ; Increase by one each time
+                     (+ password-attempts 1))  
+               (if (> password-attempts 7)     ; Check to see if we should call the cops.
                    call-the-cops
                    display-incorrect))))
   dispatch)
   
 ; don't modify this
+; Not sure why but I had to add the useless parameter to get tests 19 & 20 to pass.
+; Since it kept complaining about the number of parameters.
 (define (call-the-cops useless)
   "your account has been locked. please call customer service.")
   
