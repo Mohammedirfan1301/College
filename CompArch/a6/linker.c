261e2e63ad4495492fc06f86743f0acb4b05a8aa
Jason Downing <jdown1994@gmail.com>
2016-04-18 20:11:02 -0400

    whatever, a6 it is

---
+++ CompArch/a6/linker.c
@@ -0,0 +1,38 @@
+/*
+ *  Jason Downing
+ *  4/18/2016
+ *  jason_downing@student.uml.edu
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void output_exec();   // Functions to output to either exec or obj.
+void output_obj();
+
+// Main
+int main (int argc, char* argv[]) {
+  // Get the command line argument
+  if (argc == 1) {
+    // -o etc.
+  }
+
+  // Output the mic1 image.
+  output_exec();
+
+  // Or output to an object file?
+  outout_obj();
+
+  return 0;
+}
+
+// Output image.
+void output_exec() {
+  return;
+}
+
+// Obj file
+void object_file() {
+  return;
+}
