e64720a4e1be52263e7f465e5157c5d2c9d327ee
Jason Downing <jason_downing@student.uml.edu>
2016-09-13 14:45:20 -0400

    more progress!

--- Operating_Systems/a1/a1.c
+++ Operating_Systems/a1/a1.c
@@ -12,8 +12,11 @@
 #include <signal.h>
 #include <sys/resource.h>
 
+int counter = 0, counter_2G = 0;
+
+// your signal handler function, includes execl call
 void sigfunc (int signum) {
-  // your signal handler function, includes execl call
+  // sigfunc must load prof program with execl
 }
 
 void sig_handler(int signal) {
@@ -92,18 +95,38 @@ int main (void) {
     printf("PROG:  Process priority is:\t%d\n", priority);
 
     // child must write pipe with write call
+      /* Child process closes up input side of pipe */
+      close(fd[0]);
+
+      /* Send "string" through the output side of pipe */
+      write(fd[1], string, (strlen(string)+1));
+      exit(0);
+
     // child enters endless loop
+    while (counter_2G < 10) {
+      counter ++;
+      if (counter < 0) {
+        counter = 0;
+        counter_2G++;
+      }
+    }
   }
 
   // Parent Process
   else {
+
+    /* Parent process closes up output side of pipe */
+    close(fd[1]);
+
+    /* Read in a string from the pipe */
+    nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
+    printf("Received string: %s", readbuffer);
+
     // parent wakes from pipe read (after child writes)
     // parent sends SIGTERM to child pid with kill call
     // parent blocks on wait call
 
         // child moves to sigfunc when SIGTERM arrives
-
-    // sigfunc must load prof program with execl
 
     // prof program will print out credentials
     // prof program installs its own signal handler
