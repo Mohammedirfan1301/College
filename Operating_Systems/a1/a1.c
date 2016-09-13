af80ceebb4b397fa7c1f1545a2d1f232634746cc
Jason Downing <jason_downing@student.uml.edu>
2016-09-13 14:36:30 -0400

    making some progress on OS HW

--- Operating_Systems/a1/a1.c
+++ Operating_Systems/a1/a1.c
@@ -12,36 +12,33 @@
 #include <signal.h>
 #include <sys/resource.h>
 
-int counter = 0, counter_2G = 0;
+void sigfunc (int signum) {
+  // your signal handler function, includes execl call
+}
 
 void sig_handler(int signal) {
   printf("\n\nPROF PROG: Awake in handler - You Got Me With Signal\n");
-  printf("Number %d after spinning for more\n than %d %s loop iterations, goodbye\n\n",
-          signal, ((counter_2G)?counter_2G*2:counter), ((counter_2G)?"Billion":"\0"));
   exit(1);
 }
 
-int main(void)
-{
+int main (void) {
+
+  //****************************************************************************
+  //                      create pipe with pipe call
+  //****************************************************************************
+  int     fd[2], nbytes;
+  pid_t   childpid;
+  char    string[] = "Hello, world!\n";
+  char    readbuffer[80];
+
+  pipe(fd);
+
+  //****************************************************************************
+  //                      print out your credentials
+  //****************************************************************************
   pid_t pid, ppid;
   int ruid, rgid, euid, egid;
   int priority;
-  sigset_t  mask, proc_mask;
-  struct sigaction new;
-
-  sigemptyset(&proc_mask);                     /** clear signal mask **/
-  sigprocmask(SIG_SETMASK, &proc_mask, NULL);
-
-  sigemptyset(&mask);
-  new.sa_mask = mask;
-  new.sa_handler = sig_handler;
-  new.sa_flags = 0;
-
-  // Error check
-  if(sigaction(SIGTERM, &new, NULL) == -1) {
-    perror("failed in sigaction:");
-    exit(2);
-  }
 
   pid = getpid();
   ppid = getppid();
@@ -52,27 +49,76 @@ int main(void)
   nice(5);
   priority = getpriority(PRIO_PROCESS, 0);
 
-  printf("\nThis is the Prof process report:\n\n");
-  printf("PROF PROG:  Process ID is:\t\t%d\n", pid);
-  printf("PROF PROG:  Process parent ID is:\t%d\n", ppid);
-  printf("PROF PROG:  Real GID is:\t\t%d\n", rgid);
-  printf("PROF PROG:  Real UID is:\t\t%d\n", ruid);
-  printf("PROF PROG:  Effective GID is:\t\t%d\n", egid);
-  printf("PROF PROG:  Effective UID is:\t\t%d\n", euid);
-  printf("PROF PROG:  Process priority is:\t%d\n\n", priority);
+  printf("PARENT Credentials:\n");
+  printf("PROG:  Process ID is:\t\t%d\n", pid);
+  printf("PROG:  Process parent ID is:\t%d\n", ppid);
+  printf("PROG:  Real GID is:\t\t%d\n", rgid);
+  printf("PROG:  Real UID is:\t\t%d\n", ruid);
+  printf("PROG:  Effective GID is:\t%d\n", egid);
+  printf("PROG:  Effective UID is:\t%d\n", euid);
+  printf("PROG:  Process priority is:\t%d\n\n", priority);
 
-  printf("PROF PROG: going into and endless loop, use\n");
-  printf("kill %d  to kill me now\n\n", pid);
-  fflush(stdout);
-
-  while (counter_2G < 10) {
-    counter ++;
-    if (counter < 0) {
-      counter = 0;
-      counter_2G++;
-    }
+  //****************************************************************************
+  //                      fork child, block on pipe with read call
+  //****************************************************************************
+  if((childpid = fork()) == -1)
+  {
+    perror("fork");
+    exit(1);
   }
 
-  write(1,"PROF PROG: timed out after 20 billion iterations\n", 51);
-  exit(2);
+  // Child Process
+  if(childpid == 0) {
+    // child installs sigfunc with sigaction call
+
+    // Gathering credentials
+    pid = getpid();
+    ppid = getppid();
+    rgid = getgid();
+    ruid = getuid();
+    egid = getegid();
+    euid = geteuid();
+    nice(5);
+    priority = getpriority(PRIO_PROCESS, 0);
+
+    // child must print out credentials
+    printf("CHILD Credentials:\n");
+    printf("PROG:  Process ID is:\t\t%d\n", pid);
+    printf("PROG:  Process parent ID is:\t%d\n", ppid);
+    printf("PROG:  Real GID is:\t\t%d\n", rgid);
+    printf("PROG:  Real UID is:\t\t%d\n", ruid);
+    printf("PROG:  Effective GID is:\t%d\n", egid);
+    printf("PROG:  Effective UID is:\t%d\n", euid);
+    printf("PROG:  Process priority is:\t%d\n", priority);
+
+    // child must write pipe with write call
+    // child enters endless loop
+  }
+
+  // Parent Process
+  else {
+    // parent wakes from pipe read (after child writes)
+    // parent sends SIGTERM to child pid with kill call
+    // parent blocks on wait call
+
+        // child moves to sigfunc when SIGTERM arrives
+
+    // sigfunc must load prof program with execl
+
+    // prof program will print out credentials
+    // prof program installs its own signal handler
+    // prof program prompts user for kill command
+    // prof program enters endless loop
+
+      // user enters shell kill command
+
+    // prof program enters signal handler, will exit
+  }
+
+  // parent wakes from wait call when child dies
+  // parent prints child term status and finishes
+
+    // Print child term status?
+
+  return 0;
 }
