8098dbd41c33b5fef6a3244a3023cbb4e6814659
Jason Downing <jason_downing@student.uml.edu>
2016-12-01 10:46:09 -0500

    starting hw6

---
+++ OS/a6/main.c
@@ -0,0 +1,50 @@
+/**
+ *     Jason Downing
+ *     Assignment 6 main c file
+ */
+#include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <time.h>
+
+// Print function, displays all the stats about the files
+void print_stats(struct stat statBuffer);
+
+// Command line program
+int main(int argc, char *argv[]) {
+
+  struct stat statBuffer;
+  struct dirent* entry;
+  DIR* directory;
+  int x, y;
+
+  // Two ways to run this program: no arguments, or multiple arguments
+
+  // Multiple arguments
+  if (argc > 1) {
+
+    // Let's run through all the arguments
+    for(x = 0; x < argc; x++) {
+      printf("WE GOT AN ARGUMENT #%d\n", x);
+
+      // Should print details
+
+    }
+  }
+  // No arguments
+  else {
+
+  }
+
+  return 0;
+}
+
+// Print function, displays all the stats about the files
+void print_stats(struct stat statBuffer) {
+
+}
